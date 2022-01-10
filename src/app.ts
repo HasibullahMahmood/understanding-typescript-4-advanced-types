ae774974e6e36399249522e6b7beacc0163e2334
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-10 21:33:55 +0300

    6.4 discriminated unions

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,37 @@ const e1: Admin & Employee = {
 	privileges: ['create-server'],
 	startDate: new Date(),
 };
+
+// types
+// primitive type -> typeof
+// object -> in
+// class -> instanceof
+
+// discriminated unions
+
+interface Bird {
+	type: 'bird';
+	flyingSpeed: number;
+}
+
+interface Horse {
+	type: 'horse';
+	runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+	let speed: number;
+	switch (animal.type) {
+		case 'bird':
+			speed = animal.flyingSpeed;
+			break;
+		case 'horse':
+			speed = animal.runningSpeed;
+			break;
+	}
+	console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'horse', runningSpeed: 1000 });
